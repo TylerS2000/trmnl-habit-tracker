6300f6e3160ea928e1999ce2e64e9288f5a01467
TylerS2000 <tylerschulten@gmail.com>
2026-01-05 21:12:02 -0500

    adds port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 console.log("hi")
 const app = express()
+const port = process.env.PORT || 4000 
 
 app.get('/', (req, res)=>{
     res.send("hello world");
 })
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     return console.log("Listening on port 3000")
 })
